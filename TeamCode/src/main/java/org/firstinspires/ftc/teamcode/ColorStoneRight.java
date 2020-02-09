6c4ef4a7c83f9fcc3a65260cd85eed184b1f31f2
Gabe Banks <gabriel.t.banks@gmail.com>
2020-02-09 18:23:12 -0500

    cleaned up ColorStoneRight by removing unnessecary comments and tweaking variable names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorStoneRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorStoneRight.java
@@ -15,101 +15,93 @@ import java.util.ArrayList;
 
 public class ColorStoneRight extends OpMode {
     DcMotor frontRight, frontLeft, backRight, backLeft, extendArm;
-    //CRServo claw1, claw2;
     CRServo claw1, claw2;
-    //Servo pickUp1, pickUp2,
-    //ModernRoboticsI2cRangeSensor SenseFront, SenseLeft, SenseRight,SenseFront
-    ColorSensor mrSensor;
-    Servo mrServo;
-    DcMotor raiseArm2;
+    ColorSensor cSensor;
+    Servo cSensorServo;
+    DcMotor raiseArmMotor;
 
-    oneServo sensorDown;
-    timeState forward;
-    driveState strafeLeft;
-    extendArmState reachOut;
     private StateMachine machine;
-    clampDriveState strafeRight;
-    timeState turnLeft;
-    CRServoState2 close;
-    CRServoState open;
-    CRServoState2 close2;
-    timeState park;
+
+    //In order of execution
+    oneServo sensorDown;
+    driveState strafeLeft;
     ColorState colorState;
-    ColorState colorState2;
-    timeState backwards;
-    timeState backwards2;
-    driveState strafeRightAgain;
     timeState wait;
-    extendArmState reachOut2;
+    timeState backwards;
+    driveState strafeLeft2;
+    extendArmState reachOut;
+    CRServoState2 closeClamp;
+
+    clampDriveState strafeRight;
+    CRServoState2 closeClamp2;
+    timeState forward;
+
     extendArmState raiseArm;
+    extendArmState reachOut2;
+    CRServoState open;
+    timeState backwards2;
 
 
     ArrayList<Servo> servoPickUp= new ArrayList<Servo>();
-
     ArrayList<DcMotor> motors = new ArrayList<DcMotor>();
     ArrayList<CRServo> crServos;
 
     {
         crServos = new ArrayList<CRServo>();
     }
-    //ArrayList<ModernRoboticsI2cRangeSensor> mrrs = new ArrayList<ModernRoboticsI2cRangeSensor>();
 
 
     @Override
     public void init() {
 
-        frontRight=hardwareMap.dcMotor.get("front right");
-        frontLeft=hardwareMap.dcMotor.get("front left");
-        backRight=hardwareMap.dcMotor.get("back right");
-        backLeft=hardwareMap.dcMotor.get("back left");
-        mrSensor=hardwareMap.colorSensor.get("mrSensor");
-        mrServo=hardwareMap.servo.get("mrServo");
-        raiseArm2 = hardwareMap.dcMotor.get("raise arm 2");
+        if (true) {
 
+            frontRight = hardwareMap.dcMotor.get("front right");
+            frontLeft = hardwareMap.dcMotor.get("front left");
+            backRight = hardwareMap.dcMotor.get("back right");
+            backLeft = hardwareMap.dcMotor.get("back left");
 
-        //claw1=hardwareMap.crservo.get("claw 1");
-        //claw2=hardwareMap.crservo.get("claw 2");
-        extendArm=hardwareMap.dcMotor.get("extend arm");
+            //TODO: Change hardware maps
+            cSensor = hardwareMap.colorSensor.get("mrSensor");
+            cSensorServo = hardwareMap.servo.get("mrServo");
+            raiseArmMotor = hardwareMap.dcMotor.get("raise arm 2");
 
-        claw1= hardwareMap.crservo.get("claw 1");
-        claw2 = hardwareMap.crservo.get("claw 2");
+            extendArm = hardwareMap.dcMotor.get("extend arm");
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            claw1 = hardwareMap.crservo.get("claw 1");
+            claw2 = hardwareMap.crservo.get("claw 2");
 
-        motors.add(frontLeft);
-        motors.add(frontRight);
-        motors.add(backLeft);
-        motors.add(backRight);
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        crServos.add(claw1);
-        crServos.add(claw2);
+            motors.add(frontLeft);
+            motors.add(frontRight);
+            motors.add(backLeft);
+            motors.add(backRight);
 
+            crServos.add(claw1);
+            crServos.add(claw2);
+        }
 
-        sensorDown = new oneServo(500, 0, mrServo);
+        sensorDown = new oneServo(500, 0, cSensorServo);
         strafeLeft = new driveState(30, .3, motors, "strafeLeft");
-        colorState = new ColorState(motors, mrSensor,"backward","alpha",4500);
+        colorState = new ColorState(motors, cSensor,"backward","alpha",4500);
         wait = new timeState(500, 0, motors, "backward");
         backwards = new timeState(500, .5, motors, "backward");
-        /*colorState2 = new ColorState(motors, mrSensor,"forward","alpha",800);
-        backwards2 = new timeState(400, .5, motors, "backward");*/
-        strafeRightAgain = new driveState(16, .5, motors, "strafeLeft");
+        strafeLeft2 = new driveState(16, .5, motors, "strafeLeft");
         reachOut = new extendArmState(1200, -.5, extendArm);
-        close = new CRServoState2(1500,-1,1, crServos);
+        closeClamp = new CRServoState2(1500,-1,1, crServos);
         strafeRight = new clampDriveState(18,.5, motors, "strafeRight", -1, 1, crServos);
-        close2 = new CRServoState2(1500, -1, 1, crServos);
-        //turnLeft = new timeState(2000, .5, motors, "turnLeft");
+        closeClamp2 = new CRServoState2(1500, -1, 1, crServos);
 
         forward = new timeState(3500, .5, motors, "forward");
 
-        //TODO: this time needs to be longer:
-        raiseArm = new extendArmState(500, -1, raiseArm2);
-
-        backwards2 = new timeState(1500, .5, motors, "backward");
+        //TODO: this time needs to be adjusted:
+        raiseArm = new extendArmState(500, -1, raiseArmMotor);
         reachOut2 = new extendArmState(2000, -.5, extendArm);
-
         open = new CRServoState(1000, 1, -1, crServos);
-        park = new timeState (700, .5, motors, "backward");
+        backwards2 = new timeState(1500, .5, motors, "backward");
+
 
 
 
@@ -123,27 +115,20 @@ public class ColorStoneRight extends OpMode {
         strafeLeft.setNextState(colorState);
         colorState.setNextState(wait);
         wait.setNextState(backwards);
-        backwards.setNextState(strafeRightAgain);
+        backwards.setNextState(strafeLeft2);
+        strafeLeft2.setNextState(reachOut);
+        reachOut.setNextState(closeClamp);
+        closeClamp.setNextState(strafeRight);
 
-        /*colorState2.setNextState(backwards2);
-        backwards2.setNextState(strafeRightAgain);*/
-        strafeRightAgain.setNextState(reachOut);
-        reachOut.setNextState(close);
-        close.setNextState(strafeRight);
-        strafeRight.setNextState(close2);
-       // close2.setNextState(turnLeft);
-        //turnLeft.setNextState(forward);
-        close2.setNextState(forward);
-
-
-
+        strafeRight.setNextState(closeClamp2);
+        closeClamp2.setNextState(forward);
         forward.setNextState(raiseArm);
+
         raiseArm.setNextState(reachOut2);
         reachOut2.setNextState(open);
         open.setNextState(backwards2);
         backwards2.setNextState(null);
 
-        //park.setNextState(null);
 
 
 
